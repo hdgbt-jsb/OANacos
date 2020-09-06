535ae4d5bcc82bf4afb9f0c5a0cfc7b28c877474
jiangshuang <txtAPOTOX4869>
2020-09-06 19:47:46 +0800

    fix-gateway配置

--- oa-gateway/src/main/java/com/js/entity/RouteEntity.java
+++ oa-gateway/src/main/java/com/js/entity/RouteEntity.java
@@ -1,21 +1,21 @@
-package com.js.entity;
-
-import lombok.Data;
-
-/**
- * @program: OANacos
- * @Date: 2020/8/13 13:53
- * @Author: jiangshuang
- * @Description:
- */
-@Data
-public class RouteEntity {
-
-    private String id;
-
-    private PredicateEntity[] predicates;
-
-    private FilterEntity[] filters;
-
-    private String uri;
-}
+//package com.js.entity;
+//
+//import lombok.Data;
+//
+///**
+// * @program: OANacos
+// * @Date: 2020/8/13 13:53
+// * @Author: jiangshuang
+// * @Description:
+// */
+//@Data
+//public class RouteEntity {
+//
+//    private String id;
+//
+//    private PredicateEntity[] predicates;
+//
+//    private FilterEntity[] filters;
+//
+//    private String uri;
+//}
